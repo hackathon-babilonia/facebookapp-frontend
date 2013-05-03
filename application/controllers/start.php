4d003303532cf25157cca54c975f9e99859307d1
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-05-03 17:20:23 -0300

    CodeIgniter Framework implemented

---
+++ application/controllers/start.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Start extends CI_Controller 
+{
+
+	public function index()
+	{
+		$this->load->view('start');
+	}
+}
+
+/* End of file start.php */
+/* Location: ./application/controllers/start.php */
